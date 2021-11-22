32f0e8bd171812867edf72e746f9b3bfb80a98e9
SergioMadrid135 <androidcoc2015@gmail.com>
2021-11-22 12:33:12 +0100

    Update instagram.js

--- commands/redes sociales/instagram.js
+++ commands/redes sociales/instagram.js
@@ -9,7 +9,6 @@ module.exports = {
 
     let Insta = new MessageEmbed()
       .setTitle(`Instagram Nereko`)
-      .setThumbnail('https://scontent-mad1-1.cdninstagram.com/v/t51.2885-19/s150x150/142581213_461615878190076_985637522715823185_n.jpg?tp=1&_nc_ht=scontent-mad1-1.cdninstagram.com&_nc_ohc=UfasEYZw0-IAX-VLXuX&oh=3140fb3acc291aec72b4919c4c38be6b&oe=606E0136')
       .setDescription("https://www.instagram.com/_nereko_/")
       .setColor("#CC6721");
 
