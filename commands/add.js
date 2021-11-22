4c47e1b50967d5390650f7d6d9e9c904a0fbd736
SergioMadrid135 <androidcoc2015@gmail.com>
2021-11-22 12:44:19 +0100

    Update add.js

--- commands/add.js
+++ commands/add.js
@@ -1,7 +1,7 @@
 module.exports = {
   commands: ['add', 'addition'],
   expectedArgs: '<num1> <num2>',
-  permissionError: 'You need admin permissions to run this command',
+  permissionError: 'Necesita permisos de administrador para ejecutar este comando',
   minArgs: 2,
   maxArgs: 2,
   callback: (message, arguments, text) => {
