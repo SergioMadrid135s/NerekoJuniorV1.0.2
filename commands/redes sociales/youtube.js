00df307074fa903d69b949ccb5dba1f0b5efe2e9
SergioMadrid135 <androidcoc2015@gmail.com>
2021-11-22 12:26:46 +0100

    Update youtube.js

--- commands/redes sociales/youtube.js
+++ commands/redes sociales/youtube.js
@@ -9,7 +9,6 @@ module.exports = {
 
     let Youtube = new MessageEmbed()
       .setTitle(`Youtube Nereko`)
-      .setThumbnail('https://yt3.ggpht.com/ytc/AAUvwngsG_cVpzIe7yxEEDClOQMNyyhO3X73zW6r6EDAxg=s900-c-k-c0x00ffffff-no-rj')
       .setDescription("https://www.youtube.com/channel/UCG6NmtUZBMU4D1NRU3S6Qdg/videos?view_as=subscriber")
       .setColor("#c4302b");
 
