f043c8f5830aa53f175417e56903eb884a03f945
SergioMadrid135 <androidcoc2015@gmail.com>
2023-03-23 08:47:07 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'message',
 	execute(message) {
-		console.log(`${message.author.tag} de #${message.channel.name} enviado: ${message.content}`);
+		console.log(`${message.author.tag} de #${message.channel.name} mensaje que se envio: ${message.content}`);
 	},
 };
