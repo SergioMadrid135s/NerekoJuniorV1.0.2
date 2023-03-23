8383df70d54877a76589761eadcc121f3b083251
SergioMadrid135 <androidcoc2015@gmail.com>
2023-03-23 08:39:43 +0100

    Update help.js

--- commands/help/help.js
+++ commands/help/help.js
@@ -8,32 +8,32 @@ module.exports = {
   callback(message) {
     let commands = message.client.commands;
 
-    let help = new MessageEmbed()
-      .setTitle(`Comandos de NerekoJunior`)
-      .setThumbnail('https://images-ext-2.discordapp.net/external/nYTDfPFgvtFst8FwYGxvtC1jBJVlBkaFH9YlpIgabNk/%3Fsize%3D1024/https/cdn.discordapp.com/icons/777988362921443368/6409c8b8c634a4167e95902597c315e8.png')
-      .setDescription("Lista de Comandos")
-      .setURL('https://github.com/SergioMadrid135s')
-      .addField(` ------------------------------ Redes Sociales ---------------------------`)
-      .addField(`→ { prefix }instagram`, `Mi perfil de Instagram.`, true)
-      .addField(`→ { prefix }twitter`, `Mi perfil de Twitter.`, true)
-      .addField(`→ { prefix }twitch`, `Mi perfil de Twitch.`, true)
-      .addField(`→ { prefix }youtube`, `Mi canal de Youtube.`, true)
-      .addField(` ----------------------------- Utilidades Twitch --------------------------`)
-      .addField(`→ { prefix }cofres`, `Mi colección de cofres.`)
-      .addField(` ------------------------------- Utilidades Bot ------------------------`)
-      .addField(`→ { prefix }ping`, `Comprueba la latencia del bot.`)
-      .addField(`→ { prefix }user`, `Saber información de tu perfil discord.`)
-      .addField(`→ { prefix }server`, `Saber información del server.`)
-      .addField(`→ { prefix }avatar`, `Conseguir tu foto o de otro usuario.`)
-      .addField(`→ { prefix }meme`, `Envia un meme aleatorio.`)
-      .addField(`→ { prefix }creador`, `Usuario del creador del bot.`)
-      .addField(`→ { prefix }dueña`, `Usuario de dueño del server.`)
-      .setFooter("Pedido por" + message.author.tag)
-      .setColor("#4900FF");
+    let ayuda = new MessageEmbed()
+      ayuda.setTitle(`Comandos de NerekoJunior`)
+      ayuda.setThumbnail('https://images-ext-2.discordapp.net/external/nYTDfPFgvtFst8FwYGxvtC1jBJVlBkaFH9YlpIgabNk/%3Fsize%3D1024/https/cdn.discordapp.com/icons/777988362921443368/6409c8b8c634a4167e95902597c315e8.png')
+      ayuda.setDescription("Lista de Comandos")
+      ayuda.setURL('https://github.com/SergioMadrid135s')
+      ayuda.addField(` ------------------------------ Redes Sociales ---------------------------`)
+      ayuda.addField(`→ { prefix }instagram`, `Mi perfil de Instagram.`, true)
+      ayuda.addField(`→ { prefix }twitter`, `Mi perfil de Twitter.`, true)
+      ayuda.addField(`→ { prefix }twitch`, `Mi perfil de Twitch.`, true)
+      ayuda.addField(`→ { prefix }youtube`, `Mi canal de Youtube.`, true)
+      ayuda.addField(` ----------------------------- Utilidades Twitch --------------------------`)
+      ayuda.addField(`→ { prefix }cofres`, `Mi colección de cofres.`)
+      ayuda.addField(` ------------------------------- Utilidades Bot ------------------------`)
+      ayuda.addField(`→ { prefix }ping`, `Comprueba la latencia del bot.`)
+      ayuda.addField(`→ { prefix }user`, `Saber información de tu perfil discord.`)
+      ayuda.addField(`→ { prefix }server`, `Saber información del server.`)
+      ayuda.addField(`→ { prefix }avatar`, `Conseguir tu foto o de otro usuario.`)
+      ayuda.addField(`→ { prefix }meme`, `Envia un meme aleatorio.`)
+      ayuda.addField(`→ { prefix }creador`, `Usuario del creador del bot.`)
+      ayuda.addField(`→ { prefix }dueña`, `Usuario de dueño del server.`)
+      ayuda.setFooter("Pedido por" + message.author.tag)
+      ayuda.setColor("#4900FF");
 
 
-    help.setTimestamp();
+    ayuda.setTimestamp();
 
-    return message.channel.send(help).catch(console.error);
+    return message.channel.send(ayuda).catch(console.error);
   }
 };
